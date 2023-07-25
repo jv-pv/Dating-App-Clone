490f192bb028adba74173fc6f9a8cf0662f72879
John Pieri <jvpieri@gmail.com>
2023-07-25 19:38:24 -0400

    Changed loopBack func and reformated syntax

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
-import dogsData from "./data.js"
-import Dog from './Dog.js'
-const likeBadge = document.querySelector('.badge-like')
-const dislikeBadge = document.querySelector('.badge-dislike')
-const likeBtn = document.querySelector('.like-btn')
-const dislikeBtn = document.querySelector('.dislike-btn')
-const fadeContainer = document.querySelector('.fade-container')
-const logo = document.querySelector('.logo')
-let currentDogIndex = 0
-let currentDog = new Dog(dogsData[currentDogIndex])
+import dogsData from "./data.js";
+import Dog from "./Dog.js";
+const likeBadge = document.querySelector(".badge-like");
+const dislikeBadge = document.querySelector(".badge-dislike");
+const likeBtn = document.querySelector(".like-btn");
+const dislikeBtn = document.querySelector(".dislike-btn");
+const fadeContainer = document.querySelector(".fade-container");
+const logo = document.querySelector(".logo");
+let currentDogIndex = 0;
+let currentDog = new Dog(dogsData[currentDogIndex]);
 
-likeBtn.addEventListener('click', like)
-dislikeBtn.addEventListener('click', dislike)
-logo.addEventListener('click', reRender)
+likeBtn.addEventListener("click", like);
+dislikeBtn.addEventListener("click", dislike);
+logo.addEventListener("click", reRender);
 
 function getNewDog() {
-    currentDogIndex++
-    currentDogIndex = (currentDogIndex + 1) % dogsData.length 
-    currentDog = new Dog(dogsData[currentDogIndex])
-    // loopBack()
-    render()
+  currentDogIndex++;
+  // currentDogIndex = (currentDogIndex + 1) % dogsData.length
+  currentDog = new Dog(dogsData[currentDogIndex]);
+  loopBack();
+  render();
 }
 
 function like() {
-    likeBtn.disabled = true
-    if (likeBtn) {
-        likeBadge.classList.add('active')
-        setTimeout(() => {
-            fadeContainer.classList.add('like')
-        }, 600);
-    }
+  likeBtn.disabled = true;
+  if (likeBtn) {
+    likeBadge.classList.add("active");
     setTimeout(() => {
-        likeBadge.classList.remove('active')
-    }, 500);
-    setTimeout(() => {
-        likeBtn.disabled = false
-        fadeContainer.classList.remove('like')
-        currentDog.setMatchStatus(true)
-        getNewDog()
-    }, 1000)
+      fadeContainer.classList.add("like");
+    }, 600);
+  }
+  setTimeout(() => {
+    likeBadge.classList.remove("active");
+  }, 500);
+  setTimeout(() => {
+    likeBtn.disabled = false;
+    fadeContainer.classList.remove("like");
+    currentDog.setMatchStatus(true);
+    getNewDog();
+  }, 1000);
 }
 
 function dislike() {
-    dislikeBtn.disabled = true
-    if (dislikeBtn) {
-        dislikeBadge.classList.add('active')
-        setTimeout(() => {
-            fadeContainer.classList.add('dislike')
-        }, 600);
-    }
-    setTimeout(()=> {
-        dislikeBadge.classList.remove('active')
-    }, 500)
-    setTimeout(()=> {
-        dislikeBtn.disabled = false
-        fadeContainer.classList.remove('dislike')
-        currentDog.setMatchStatus(false)
-        getNewDog()
-    }, 1000)
+  dislikeBtn.disabled = true;
+  if (dislikeBtn) {
+    dislikeBadge.classList.add("active");
+    setTimeout(() => {
+      fadeContainer.classList.add("dislike");
+    }, 600);
+  }
+  setTimeout(() => {
+    dislikeBadge.classList.remove("active");
+  }, 500);
+  setTimeout(() => {
+    dislikeBtn.disabled = false;
+    fadeContainer.classList.remove("dislike");
+    currentDog.setMatchStatus(false);
+    getNewDog();
+  }, 1000);
 }
-    
 
-// function loopBack() {
-//     let finalIndex = dogsData.length - 1
-//     if (currentDogIndex > finalIndex) {
-//         currentDogIndex = 0
-//         currentDog = new Dog(dogsData[currentDogIndex])
-//     }
-// }
+function loopBack() {
+  let finalIndex = dogsData.length - 1;
+  if (currentDogIndex > finalIndex) {
+    currentDogIndex = 0;
+    currentDog = new Dog(dogsData[currentDogIndex]);
+  }
+}
 
 function reRender() {
-    currentDogIndex = (currentDogIndex + 1) % dogsData.length
-    currentDog = new Dog(dogsData[currentDogIndex])
-    render()
+  currentDogIndex = (currentDogIndex + 1) % dogsData.length;
+  currentDog = new Dog(dogsData[currentDogIndex]);
+  render();
 }
 
 function render() {
-    document.querySelector('.fade-container').innerHTML = currentDog.setDogHtml()
+  document.querySelector(".fade-container").innerHTML = currentDog.setDogHtml();
 }
 
-render()
+render();
